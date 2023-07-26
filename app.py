6ff3898f95cedfbc085beb31c746ff1020563073
setveNguyen1206 <thepdatoipm@gmail.com>
2023-07-26 08:21:41 +0700

    form

--- app.py
+++ app.py
@@ -36,12 +36,28 @@ def get_user():
     # print(users)
     return render_template('users.html', users=users)
 
+@app.route("/api/users")
+def api_get_user():
+    users = get_all_users()
+    # print(users)
+    return jsonify(users)
+
 @app.route("/add-user", methods=['post'])
 def add_user_route():
     data=request.form
     # print(data['gender'])
     add_user(data)
     return redirect(url_for('get_user'))
+
+@app.route('/survey', methods = ['POST', 'GET'])
+def render_survey():
+    if request.method == 'POST':
+        return jsonify({'test': 'pass'})
+    elif request.method == 'GET':
+        return render_template('survey.html')
+    
+
+
 
 
 if __name__ == "__main__":
