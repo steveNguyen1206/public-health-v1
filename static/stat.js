dde6993b7cef78cbf9a148b90a6bb4dc146d1ba0
Khang0608 <hoduykhanghocmai@gmail.com>
2023-08-03 15:26:54 +0700

    for Kiet

--- static/stat.js
+++ static/stat.js
@@ -6,41 +6,43 @@ const general_ward = document.querySelector('#btn-check-outlined-3')
 const ward = document.querySelector('#wards_stat')
 const view_stat_btn = document.querySelector('#view_stat_btn-3823')
 
-function updateChart(jdata) {
-  let m_dataPoints = [];
+// function updateChart(jdata) {
+//   let m_dataPoints = [];
 
-  jdata.forEach(item => {
-    m_dataPoints.push({ y: parseInt(item.y),x: item.label });
-  });
+//   jdata.forEach(item => {
+//     m_dataPoints.push({ y: parseInt(item.y),x: item.label });
+//   });
 
-  var chart = new CanvasJS.Chart("chart_wrapper-3823", {
-    title: {
-      text: "Dân số theo mỗi quận"
-    },
-    // axisY: {
-    //   title: "Địa đ",
-    //   suffix: "người"
-    // },
-    data: [{
-      type: "pie",	
-      showInLegend: true,
-      yValueFormatString: "# người",
-      indexLabel: "{label} {y}",
-      legendText: "{y}",
-      dataPoints: m_dataPoints
-    }]
-  });
+//   var chart = new CanvasJS.Chart("chart_wrapper-3823", {
+//     title: {
+//       text: "Dân số theo mỗi quận"
+//     },
+//     // axisY: {
+//     //   title: "Địa đ",
+//     //   suffix: "người"
+//     // },
+//     data: [{
+//       type: "pie",	
+//       showInLegend: true,
+//       yValueFormatString: "# người",
+//       indexLabel: "{y}",
+//       legendText: "{label}",
+//       dataPoints: [{'label': 'Quận Bình Thạnh', 'y': 2}, {'label': 'Quận Tân Phú', 'y': 2}, {'label': 'Quận 12', 'y': 1}, {'label': 'Thành phố Thủ Đức', 'y': 3}]
+//     }]
+//   });
 
-	var deltaY, yVal;
-	var dps = chart.options.data[0].dataPoints;
-	for (var i = 0; i < dps.length; i++) {
-		deltaY = Math.round(2 + Math.random() *(-2-2));
-		yVal = deltaY + dps[i].y > 0 ? dps[i].y + deltaY : 0;		
-		dps[i] = {y: yVal};
-	}
-	chart.options.data[0].dataPoints = dps; 
-	chart.render();
-}
+// 	var deltaY, yVal;
+// 	var dps = chart.options.data[0].dataPoints;
+// 	for (var i = 0; i < dps.length; i++) {
+// 		deltaY = Math.round(2 + Math.random() *(-2-2));
+// 		yVal = deltaY + dps[i].y > 0 ? dps[i].y + deltaY : 0;		
+// 		dps[i] = {y: yVal};
+// 	}
+// 	chart.options.data[0].dataPoints = dps; 
+// 	chart.render();
+// }
+
+
 
 view_stat_btn.addEventListener('click', function() {
   
